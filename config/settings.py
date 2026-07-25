da3d5417d0c05488e52d39f9b8313a2ffb0e961a
Mbitila <mbitila@gmail.com>
2026-07-25 15:52:24 +0300

    Configure allowed hosts for local network testing

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-=2f3=^e3+8qt8@s^6!tsz)&zxnuu*$fg6jz4rc)$)!jx&)c$es
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
 
 
 # Application definition
