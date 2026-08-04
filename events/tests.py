4d1c7160b9f0657fd580b3b80a9e23055c5bd489
Mbitila <mbitila@gmail.com>
2026-08-04 23:30:33 +0300

    Add registration status to public navigation

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class PublicNavigationTests(TestCase):
+    def test_public_navigation_links_to_registration_status(self):
+        response = self.client.get("/en/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Check registration status")
+        self.assertContains(response, "/en/registration-status/")
